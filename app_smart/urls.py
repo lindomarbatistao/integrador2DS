950c3cae60a36d3dab88eded3f124d7f45aed9ea
Lindomar <lindomarbatistao@gmail.com>
2024-10-10 07:30:08 -0300

    10-10-24 7:30

--- app_smart/urls.py
+++ app_smart/urls.py
@@ -4,7 +4,7 @@ from app_smart.api.viewsets import CreateUserAPIViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from app_smart.api.viewsets import CreateUserAPIViewSet
 from rest_framework.routers import DefaultRouter
-from .views import upload_csv_view, load_temperature_data
+from .views import upload_csv_view, load_temperature_data, upload_csv_view_test
 from app_smart.api.filters import SensorFilterView
 
 from app_smart.api.viewsets import (
@@ -25,6 +25,7 @@ urlpatterns = [
  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
  path('api/', include(router.urls)),
  path('api/upload_csv/', upload_csv_view, name='upload_csv'),
+ path('api/test/', upload_csv_view_test, name='upload_csv'),
  path('api/sensor_filter/', SensorFilterView.as_view(), name='sensor_filter'),
  path('api/temperatura/', load_temperature_data, name='temperatura_data')
 ]
--- app_smart/views.py
+++ app_smart/views.py
@@ -98,28 +98,23 @@ def load_temperature_data(request):
 
 
 
+def upload_csv_view_test(request):
+    if request.method == 'POST':
+        form = CSVUploadForm(request.POST, request.FILES)
+        form_temperature = CSVUploadTemp(request.POST, request.FILES)
+        
+        if form.is_valid():
+            csv_file = request.FILES['file']
+            # Código existente para processar CSV de sensores...
+        elif form_temperature.is_valid():
+            csv_file = request.FILES['file']
+            # Código existente para processar CSV de temperatura...
+        else:
+            form = CSVUploadForm()
+            form_temperature = CSVUploadTemp()
 
+    else:
+        form = CSVUploadForm()
+        form_temperature = CSVUploadTemp()
 
-# Configuração do Django
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_city.settings') 
-# django.setup()
-
-# def load_temperature_data(csv_file_path):     
-#     print("Início da importação:", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))     
-#     with open(csv_file_path, newline='', encoding='utf-8') as csvfile:         
-#         reader = csv.DictReader(csvfile)         
-#         line_count = 0         
-#         for row in reader:             
-#             sensor_id = int(row['sensor_id'])             
-#             valor = float(row['valor'])
-#             timestamp = parser.parse(row['timestamp'])  
-#             # Usa dateutil para analisar a data com fuso horário
-#             sensor = Sensor.objects.get(id=sensor_id)
-#             TemperaturaData.objects.create(sensor=sensor, valor=valor, timestamp=timestamp)             
-#             line_count += 1             
-#             if line_count % 10000 == 0:                 
-#                 print(f"{line_count} linhas processadas...")
-#     print("Fim da importação:", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-#     print(f"Dados carregados com sucesso de {csv_file_path}")
-# # Chame a função para carregar os dados do arquivo CSV 
-# load_temperature_data('dados/temperatura_data.csv')
+    return render(request, 'app_smart/upload_csv.html', {'form_sensors': form, 'form_temperature': form_temperature})
